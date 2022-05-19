f4facf1f015251869d8c84f46e378aec02714343
OJailson17 <JaylsonO17@gmail.com>
2022-05-18 21:19:40 -0300

    fix creation user test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,38 +1,44 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let usersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
 
 describe("Create a new user", () => {
   beforeEach(() => {
     usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
   });
 
   it("should be able to create a new user", async () => {
-    const user = await usersRepository.create({
+    const user = {
       name: "Test name",
       email: "test@test.com",
       password: "test123",
-    });
+    };
 
-    expect(user).toHaveProperty("id");
+    const response = await createUserUseCase.execute(user);
+
+    expect(response).toHaveProperty("id");
   });
 
   it("should not be able to create a new user with the same email", async () => {
-    try {
-      await usersRepository.create({
+    expect(async () => {
+      const user = {
         name: "Test name",
         email: "test@test.com",
         password: "test123",
-      });
+      };
 
-      await usersRepository.create({
-        name: "Test name",
+      const user1 = {
+        name: "Test name 1",
         email: "test@test.com",
         password: "test123",
-      });
-    } catch (error) {
-      expect(error).toBeInstanceOf(CreateUserError);
-    }
+      };
+
+      await createUserUseCase.execute(user);
+      await createUserUseCase.execute(user1);
+    }).rejects.toBeInstanceOf(CreateUserError);
   });
 });
