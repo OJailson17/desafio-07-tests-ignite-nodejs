84a1e3c077c852743b8b61abea252e41204a3211
OJailson17 <JaylsonO17@gmail.com>
2022-05-19 21:52:09 -0300

    update show user profile test

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -7,14 +7,12 @@ import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 let usersRepository: InMemoryUsersRepository;
 let showUserProfileUseCase: ShowUserProfileUseCase;
 let createUserUseCase: CreateUserUseCase;
-let authenticateUserUseCase: AuthenticateUserUseCase;
 
 describe("Show user profile", () => {
   beforeEach(async () => {
     usersRepository = new InMemoryUsersRepository();
     showUserProfileUseCase = new ShowUserProfileUseCase(usersRepository);
     createUserUseCase = new CreateUserUseCase(usersRepository);
-    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
   });
 
   it("should be able to show user profile", async () => {
@@ -24,12 +22,7 @@ describe("Show user profile", () => {
       password: "test123",
     };
 
-    await createUserUseCase.execute(userData);
-
-    const { user } = await authenticateUserUseCase.execute({
-      email: userData.email,
-      password: userData.password,
-    });
+    const user = await usersRepository.create(userData);
 
     const userInfo = await showUserProfileUseCase.execute(user.id);
 
